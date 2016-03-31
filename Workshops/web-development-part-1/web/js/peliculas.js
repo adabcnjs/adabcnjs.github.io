b4c39d88ed44c85e1347c6b4618035bf3ea662af
Elena García Peña <elena.garcia@restalo.com>
2016-03-31 15:58:33 +0200

    Sort selector

--- Workshops/web-development-part-1/web/js/peliculas.js
+++ Workshops/web-development-part-1/web/js/peliculas.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 	var filmBuilder = {
-		build: function() {
-			for (var i in FILMS) {
+		films: FILMS,
+
+		show: function() {
+			for (var i in this.films) {
 				var film = FILMS[i];
 				htmlFilm = this.buildHtmlFilm(film);
 				$('.films').append(htmlFilm);
@@ -10,7 +12,6 @@ $(document).ready(function() {
 
 		buildHtmlFilm: function(film) {
 			var html = '<div class="film">';
-			console.log(film);
 			html = html.concat('<img src="'+film.Poster+'"/>');
 			html = html.concat('<div class="data">');
 			html = html.concat('<div class="title">'+film.Title+'</div>');
@@ -18,8 +19,31 @@ $(document).ready(function() {
 			html = html.concat('<div class="year">'+film.Year+'</div>');
 			html.concat('</div>');
 			return html;
+		},
+
+		clear: function() {
+			$('.films').empty();
+		},
+
+		sortFilms: function(field) {
+			this.films.sort(function(film1,film2) {
+				if (field == 'Year') {
+					return film1[field] - film2[field];
+				} else {
+					if (film1[field] < film2[field]) return -1;
+					if (film1[field] > film2[field]) return 1;
+					return 0;
+				}
+				
+			});
 		}
 	}
 
-	filmBuilder.build();
+	filmBuilder.show();
+
+	$('#filmsOrder').change(function() {
+		filmBuilder.sortFilms(this.value);
+		filmBuilder.clear();
+		filmBuilder.show();
+	})
 });
