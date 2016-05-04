26f398cf0a9917266d3c47aa5bf7b6b20b9c578a
pchiwan <pchiwan@gmail.com>
2016-05-05 00:43:27 +0200

    Create V2 of CriticadaJS, with AJAX requests to the OMDb API

---
+++ Workshops/criticadaJSv2/js/film.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+	var filmUtils = {
+		getParameterByName: function(name) {
+	    	var url = window.location.href;
+	    	name = name.replace(/[\[\]]/g, "\\$&");
+	    	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i"),
+	        	results = regex.exec(url);
+		    if (!results) return null;
+		    if (!results[2]) return '';
+		    return decodeURIComponent(results[2].replace(/\+/g, " "));
+		},
+
+		getFilmId: function() {
+			return this.getParameterByName('film');
+		},
+
+		findFilmData: function(filmId) {
+			$.get('http://www.omdbapi.com/?i=' + filmId, function (data) {
+				filmUtils.fillFilmInHtml(data);
+			});
+		},
+
+		fillFilmInHtml: function(film) {
+			$('.poster').html('<img src="'+film.Poster+'"/>');
+			$('.title').text(film.Title);
+			$('.year').text(film.Year);
+			$('.genre').text(film.Genre);
+			$('.director').text(film.Director);
+			$('.actors').text(film.Actors);
+			$('.plot').text(film.Plot);
+		}
+	};
+	
+	var filmId = filmUtils.getFilmId();
+	filmUtils.findFilmData(filmId);
+});
